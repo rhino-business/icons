bac58619c235f45f391f928e4674e3c8cee49689
rhino <rhino@mailfence.com>
2025-02-19 16:25:57 +0100

    19.02/3 add

---
+++ icons.php
@@ -0,0 +1,52 @@
+<!--
+/**
+* Tiny icons
+* Webicons / HTML icons
+*
+*
+* @ Author Jane Rhino <rhino@mailfence.com>
+* @ Github https://github.com/rhino-business/icons
+* @ Copyright 2024 rhino@mailfence.com
+* @ Link https://rhino.business
+* @ Readme  https://github.com/rhino-business/icons/blob/main/README.md
+* @ License https://github.com/rhino-business/icons/blob/main/LICENSE.md
+* @ Released on: December, 12 2024
+*/
+-->
+
+<!doctype html>
+ <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Tiny icons</title>
+    <meta name="description" content="Small tiny icons for web">
+    <link rel="stylesheet" href = "styles/icons.css">  
+  </head>
+        <body>
+              <div class = "container_huge">
+                 <div id = "badges">
+                         <section id = "age_under">
+                            <div class = "signs">
+                              <div class = "inner">
+                                 <div class = "digits">
+                                    <p>18+</p>
+                                 </div> 
+                              </div>
+                            </div>    
+                         </section>
+                          <section id = "do_not_smoke">
+                             <div class = "signs">
+                               <div class = "inner">
+                                  <div class = "roll">
+                                    <div class = "ash"></div>  
+                                  </div>
+                                    <div class = "fume_up"></div>  
+                               </div>
+                                 <div class = "strip"></div>
+                             </div> 
+                          </section>
+                     </div> 
+                </div>
+        </body>
+  </html>    
